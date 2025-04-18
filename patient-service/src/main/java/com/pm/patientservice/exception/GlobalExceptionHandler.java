eab3ede3f79a5646a220af29e540bf1571e8fc21
mannk26 <mrityunjayeng720@gmail.com>
2025-04-18 17:29:46 +0530

    started with billing-service

--- patient-service/src/main/java/com/pm/patientservice/exception/GlobalExceptionHandler.java
+++ patient-service/src/main/java/com/pm/patientservice/exception/GlobalExceptionHandler.java
@@ -1,7 +1,7 @@
 package com.pm.patientservice.exception;
 
 //import lombok.extern.slf4j.Slf4j;
-import lombok.extern.slf4j.Slf4j;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
